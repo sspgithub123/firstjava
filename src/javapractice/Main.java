ac81e74dd9c18135695607c873d9f3605f7827c4
Shubham <ssp671995@gmail.com>
2022-02-21 12:54:25 +0530

    initial commond

---
+++ src/javapractice/Main.java
@@ -0,0 +1,14 @@
+package javapractice;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.print("This sum of these number is: ");
+        int num1 = 100;
+        int num2 = 55;
+        int num3 = 785;
+        int sum = num1 + num2 + num3;
+        System.out.println(sum);
+	// write your code here
+    }
+}
